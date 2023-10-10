14f8864193aadb4fe48710056b2b34d361f0fc6c
vishvaprakash24 <vishvaprakashmaddheshiya@gmail.com>
2023-10-10 23:46:55 +0530

    inital commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    target: '#svelte',
     adapter: adapter()
   },
   preprocess: vitePreprocess()
